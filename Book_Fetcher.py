ea0a9e7a4b33ccaa3e8fe16e0fefb9a36e64082d
Aditya Diwane <55897634+adityadiwane@users.noreply.github.com>
2020-06-04 16:22:27 -0400

    Version 3
    -Previous versions inserted duplicate records in the database so fixed that issue
    -Refactored the code and created functions for repetative code

--- Book_Fetcher.py
+++ Book_Fetcher.py
@@ -3,6 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 all_books = []
+
+
 def Scrape_Books():
     # 1st run get all categories
     url = "http://books.toscrape.com/index.html"
@@ -14,46 +16,52 @@ def Scrape_Books():
     for book in books:
         category.append(str(book.find("a").get_text()).strip())
         cat_url.append(str(book.find("a")["href"]))
-    category_dict = dict(zip(category,cat_url))
+    category_dict = dict(zip(category, cat_url))
     print("CATEGORIES LIST CREATED")
-    
+
     # 2nd get books from each category
     for item in category_dict.items():
         base_url = "http://books.toscrape.com/"
         url = item[1]
-        response = requests.get(base_url+url)
+        response = requests.get(base_url + url)
         soup = BeautifulSoup(response.text, "html.parser")
         books = soup.find_all("article")
         print(f"Currently fetching {item[0]} Category")
         for book in books:
-            book_data = (get_title(book),get_price(book),item[0],get_rating(book),)
+            book_data = (
+                get_title(book),
+                get_price(book),
+                item[0],
+                get_rating(book),
+            )
             all_books.append(book_data)
-        
-        
-
 
 
 def save_books(all_books):
-	connection = sqlite3.connect("books.db")
-	c = connection.cursor()
-	c.execute('''CREATE TABLE books 
+    connection = sqlite3.connect("books.db")
+    c = connection.cursor()
+    c.execute('''CREATE TABLE books
 		(title TEXT,price REAL,category TEXT,rating INTEGER)''')
-	c.executemany("INSERT INTO books VALUES (?,?,?,?)", all_books)
-	connection.commit()
-	connection.close()
+    c.executemany("INSERT INTO books VALUES (?,?,?,?)", all_books)
+    connection.commit()
+    connection.close()
+
 
 def get_title(book):
-	return book.find("h3").find("a")["title"]
+    return book.find("h3").find("a")["title"]
+
 
 def get_price(book):
-	price = book.select(".price_color")[0].get_text()
-	return float(price.replace("£","").replace("Â",""))
+    price = book.select(".price_color")[0].get_text()
+    return float(price.replace("£", "").replace("Â", ""))
+
 
 def get_rating(book):
-	ratings = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
-	paragraph = book.select(".star-rating")[0]
-	word = paragraph.get_attribute_list("class")[-1]
-	return ratings[word]
+    ratings = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+    paragraph = book.select(".star-rating")[0]
+    word = paragraph.get_attribute_list("class")[-1]
+    return ratings[word]
+
 
 Scrape_Books()
 print("Connecting To Database....")
---
+++ Book_Fetcher_v3.py
@@ -0,0 +1,104 @@
+import sqlite3
+import requests
+import os
+from bs4 import BeautifulSoup
+from time import sleep
+import pathlib
+
+path = str(pathlib.Path(__file__).parent.absolute())
+
+all_books = []
+db = "\\books.db"
+
+
+def Scrape_Books():
+    # 1st run get all categories
+    url = "http://books.toscrape.com/index.html"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    books = soup.find(class_="nav nav-list").find("ul").find_all("li")
+    category = []
+    cat_url = []
+    for book in books:
+        category.append(str(book.find("a").get_text()).strip())
+        cat_url.append(str(book.find("a")["href"]))
+    category_dict = dict(zip(category, cat_url))
+    print("CATEGORIES LIST CREATED")
+
+    # 2nd get books from each category
+    for item in category_dict.items():
+        cnt = 1
+        url = item[1]
+        while url:
+            base_url = "http://books.toscrape.com/"
+            response = requests.get(base_url + url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            books = soup.find_all("article")
+            print(f"Currently fetching {item[0]} Category Page - {cnt}")
+            for book in books:
+                book_data = (get_title(book), get_price(
+                    book), item[0], get_rating(book),)
+                all_books.append(book_data)
+            next_butn = soup.find(class_="next")
+            url = next_butn.find("a")["href"] if next_butn else None
+            cnt += 1
+
+
+def save_books(all_books):
+    run_query(
+        "INSERT OR REPLACE INTO books(title,price,category,rating) VALUES (?,?,?,?);",
+        all_books)
+
+
+def setup_db():
+    path = str(pathlib.Path(__file__).parent.absolute())
+    path = path + db
+    #path = r"L:\\Udemy\\python_bootcamp\\{}".format(db)
+    if not os.path.isfile(path):
+        print("Creating Database")
+        open(db, 'w').close()
+        run_query(
+            "CREATE TABLE books(title TEXT,price REAL,category TEXT,rating INTEGER)")
+        run_query("CREATE UNIQUE INDEX idx_title ON books(title)")
+        print("Database Sucessfully Created")
+
+
+def get_title(book):
+    return book.find("h3").find("a")["title"]
+
+
+def get_price(book):
+    price = book.select(".price_color")[0].get_text()
+    return float(price.replace("£", "").replace("Â", ""))
+
+
+def get_rating(book):
+    ratings = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+    paragraph = book.select(".star-rating")[0]
+    word = paragraph.get_attribute_list("class")[-1]
+    return ratings[word]
+
+
+def run_query(query, values=None):
+    connection = sqlite3.connect(db)
+    c = connection.cursor()
+    if values:
+        import types
+        if not isinstance(values, str):
+            c.executemany(query, values)
+            print(f"Executing MANY:{query}")
+        else:
+            c.execute(query, values)
+            print(f"Executing:{query}")
+    else:
+        c.execute(query)
+        print(f"Executing:{query}")
+    connection.commit()
+    connection.close()
+
+
+Scrape_Books()
+setup_db()
+print("Connecting To Database....")
+save_books(all_books)
+print("Writing To Database completed")
